9da3e7bf53019076b3404dc218cd0751fe98a1cd
sreekanth mandala <sreekanthmandalaa@gmail.com>
2023-03-28 03:28:51 +0530

    added router href and deployed in firebase

--- i18nAngular/src/app/app-routing.module.ts
+++ i18nAngular/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'en-US' , component : AppComponent
+  },
+  {
+    path:'', redirectTo:'en-US' , pathMatch:'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- i18nAngular/src/app/app.component.ts
+++ i18nAngular/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'i18nAngular';
+  siteLocale:any;
+  siteLanguage:any = "English";
+  languageList =[
+    {code : "en-US" , label : "English"},
+    {code : "fr" , label : "French"},
+  ]
+
+  ngOnInit(){
+    this.siteLocale = window.location.pathname.split('/')[1];
+    this.siteLanguage = this.languageList.find(f=>f.code === this.siteLocale)?.label
+  }
 }
